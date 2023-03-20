5f34cc80a39d3f323ff8b610c38838f1566aa458
Lokesh <lokesh.b2020cce@sece.ac.in>
2023-03-20 10:37:35 +0530

    ~_~

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
         >
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
-            Lavant
+            Lavant &nbsp;
             <span className='sm:block hidden'> | Tech </span>
           </p>
         </Link>
