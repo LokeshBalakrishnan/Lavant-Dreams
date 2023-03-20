5ec6efa73dc95f66a0525f77a2be8ff3b342700e
Lokesh <lokesh.b2020cce@sece.ac.in>
2023-03-20 10:35:43 +0530

    name changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,8 +44,8 @@ const Navbar = () => {
         >
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
-            Lokesh &nbsp;
-            <span className='sm:block hidden'> | Balakrishnan</span>
+            Lavant
+            <span className='sm:block hidden'> | Tech </span>
           </p>
         </Link>
 
